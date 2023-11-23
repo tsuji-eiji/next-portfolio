6f5f0ed340f503409300522c49b996016a1030e3
Eiji Tsuji <>
2023-11-23 12:10:28 +0900

    Header 追加

---
+++ app/component/Header.jsx
@@ -0,0 +1,8 @@
+export default async function StaticPage() {
+
+  return (
+    <header>
+      <h1 className="text-center">TITLE</h1>
+    </header>
+  );
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { getList } from "../lib/microcms";
+import Header from "./component/Header";
 import ArticleList from "./component/ArticleList";
 import CategoryLinks from "./component/CategoryLinks";
 
@@ -8,6 +9,7 @@ export default async function StaticPage() {
 
   return (
     <>
+      <Header />
       <main className="flex justify-center mx-48">
         <div className="w-3/4">
           <ArticleList contents={contents} />
