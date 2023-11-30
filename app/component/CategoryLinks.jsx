32e535721087937f92081ed4b032511a42e136bc
Eiji Tsuji <>
2023-11-30 09:22:09 +0900

    カテゴリ修正

--- app/component/CategoryLinks.jsx
+++ app/component/CategoryLinks.jsx
@@ -2,17 +2,18 @@ import Link from "next/link";
 import { getCategories } from "../../lib/microcms";
 
 export default async function StaticPage() {
-  const categories = [];
+  // const categories = [];
   // 親カテゴリを抽出
-  const parents = await getCategories({ filters: 'parent[not_exists]' });
-  for (const parent of parents) {
-    // 出力用リストに親カテゴリを追加
-    categories.push(parent);
-    // 子カテゴリを抽出
-    const children = await getCategories({ filters: 'parent[equals]' + parent.id });
-    // 出力用リストに子カテゴリを追加
-    children.forEach(child => {categories.push(child)});
-  }
+  const categories = await getCategories();
+  // const parents = await getCategories({ filters: 'parent[not_exists]' });
+  // for (const parent of parents) {
+  //   // 出力用リストに親カテゴリを追加
+  //   categories.push(parent);
+  //   // 子カテゴリを抽出
+  //   const children = await getCategories({ filters: 'parent[equals]' + parent.id });
+  //   // 出力用リストに子カテゴリを追加
+  //   children.forEach(child => {categories.push(child)});
+  // }
 
   return (
     <div className="category-links">
